00a7f0e21835624b446384900fecb4cdc6ca2479
Tarun Dev <62273965+tarundev-x@users.noreply.github.com>
2023-12-06 22:43:06 +0530

    Create QuickSort.java

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,90 @@
+/*package whatever //do not write package name here */
+
+// Time Complexity:
+// Average Case: O(N * logN), where N is the length of the array.
+// Best Case: O(N * logN)
+// Worst Case: O(N2)
+// Auxiliary Space: O(1)
+// import java.io.*;//
+
+class GFG {
+    static void merge(int left,int middle,int right,int arr[])
+    {
+        int l=middle-left+1;
+        int r=right-middle;
+        
+        int larr[]=new int[l];
+        int rarr[]=new int[r];
+        
+        
+        
+        for(int i=0;i<l;i++)
+        {
+            larr[i]=arr[left+i];
+        }
+        
+        for(int j=0;j<r;j++)
+        {
+            rarr[j]=arr[middle+1+j];
+        }
+        
+       int i=0;
+       int j=0;
+        int k=left;
+        while(i<l&&j<r)
+        {
+            if(larr[i]<=rarr[j])
+            {
+                arr[k]=larr[i];
+                i++;
+            }
+            else{
+                arr[k]=rarr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<l)
+        {
+            arr[k]=larr[i];
+            k++;
+            i++;
+            
+        }
+        while(j<r)
+        {
+            arr[k]=rarr[j];
+            k++;
+            j++;
+        }
+    }
+    static void display(int arr[])
+    {
+        for(int x:arr)
+        {
+            System.out.println(x);
+        }
+    }
+    static void mergesort(int left,int right,int arr[] )
+    {
+        int middle;
+        if(left<right)
+        {
+            middle=(left+right)/2;
+            
+            mergesort(left,middle,arr);
+            mergesort(middle+1,right,arr);
+            
+            merge(left,middle,right,arr);
+            
+            
+        }
+    }
+    
+	public static void main (String[] args) {
+		int arr[]={5,1,8,3,9,2};
+		int n=arr.length;
+		GFG.mergesort(0,n-1,arr);
+		GFG.display(arr);
+	}
+}
