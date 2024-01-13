d13fd9e7685ee0e7d1143a9b6a7a1c64edee04f7
Tarun Dev <62273965+tarundev-x@users.noreply.github.com>
2024-01-13 17:56:44 +0530

    Update SingleLinkedListImplementation.java

--- SinglyLinkedList/SingleLinkedListImplementation.java
+++ SinglyLinkedList/SingleLinkedListImplementation.java
@@ -1,3 +1,4 @@
+package Collection;
 
 public class SinglyLinked {
 	
@@ -15,11 +16,12 @@ public class SinglyLinked {
         
     }
 
-    //we head node and initialize to null
+    // head node and initialize to null
     public Node head = null;
 
+    
     //adding node to head
-    public Node addNode(int d) {
+    public Node addNodeAtTail(int d) {
         if (head == null) 
         {
             head = new Node(d);
@@ -36,7 +38,35 @@ public class SinglyLinked {
         // Return the head of the linked list after adding the node
         return head;
     }
+    public void Insertbegin(int d)
+    {
+    	Node temp=new Node(d);
+    	temp.next=head;
+    	head=temp;
+    }
 
+    public void InsertMiddle(int d)
+    {
+    	Node newNode=new Node(d);
+    	int c=0;
+    	Node temp=head;
+    	while(temp!=null)
+    	{
+    		temp=temp.next;
+    		c++;
+    	}
+    	temp=head;
+
+    	for(int i=1;i<(c+1)/2;i++)
+    	{
+    		temp=temp.next;
+    	}
+    	newNode.next=temp.next;
+    	temp.next=newNode;
+
+    }
+    
+    
     public void display() 
     {
         Node temp = head;
@@ -52,21 +82,43 @@ public class SinglyLinked {
     }
 
 
+    public boolean search(int d)
+    {
+    	Node temp=head;
+    	while(temp.next!=null)
+    	{
+    		if(temp.data==d)
+    		{
+    			return true;
+    		}
+    		temp=temp.next;
+    	}
+    	return false;
+    }
     public static void main(String[] args) 
     {
         SinglyLinked lst = new SinglyLinked();
         
-        Node head = null;
-        lst.addNode(1);
-        lst.addNode(2);
-        lst.addNode(3);
-        lst.addNode(4);
-        lst.addNode(5);
-        lst.addNode(6);
-
-       
+        lst.addNodeAtTail(1);
+        lst.addNodeAtTail(2);
+        lst.addNodeAtTail(3);
+        lst.addNodeAtTail(4);
+        lst.addNodeAtTail(5);
+        lst.addNodeAtTail(6);
 
         // Display the entire linked list starting from the head
         lst.display();
+        //insert at begiing
+        System.out.println("Inserting Element at the begining");
+        lst.Insertbegin(10);
+        lst.Insertbegin(11);
+        lst.display();
+        //searcing
+       boolean ans= lst.search(5);
+       System.out.println(ans!=true?"Element Not Found":"Element Found");
+       
+       System.out.println("Inserting Element at the Midddle of the linked list");
+       lst.InsertMiddle(55);
+       lst.display();
     }
 }
